254f086e13ca54e94b3bc46f559672c452fb0a39
xaht <milaaman@rambler.ru>
2021-11-05 01:13:15 +0300

    Нужна настройка проверок

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os, eyed3, tempfile
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+"""Функция создания списка аудиофайлов"""
+def create_audiofiles_list(path_to_folder_with_files):
+    """В цикле пройти по всем файлам в папке и сохранить их названия в список"""
+    files_list = []
+    for f in os.listdir(path_to_folder_with_files):
+        file_name_with_path = f"{path_to_folder_with_files}/{f}"
+        files_list.append(file_name_with_path)
+    return (files_list)
+    #print(files_list)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def create_directory_artist_album(audiofile_path):
+    """Функция получения метаданных из аудиофайла"""
+    """Params:
+    audiofile_path:argument"""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    audiofile = eyed3.load(audiofile_path)  # "music/Madelyne - Beautiful Child (Hiver Hammer remix).mp3"
+    """Настроить условие: 
+    - если в тегах нет исполнителя - сохранить в папку исполнитель не найден, имя файла не менять;
+    - если в тегах нет альбома - сохранить в паку с названием исполнителя"""
+    print(f"Исполнитель: {audiofile.tag.artist}")
+    print(f"Альбом: {audiofile.tag.album}")
+    print(f"Альбом: {audiofile.tag.album_artist}")
+    print(f"Наименование: {audiofile.tag.title}")
+    #audiofile.tag.album = "OST Shchnazzy - Portland 2003"  # присвоить альбому значение str
+    #audiofile.tag.save()
+    #path_artist = os.mkdir(f"C:/python/tests/milaamania/music/{audiofile.tag.artist}")
+    path_album = "C:/python/tests/milaamania/music"
+    """Настроить проверку наличия директории:
+     - если есть исполнитель - провалиться в неё;
+     - если есть альбом - провалиться в него"""
+
+    make_album_folder =  os.makedirs(f"{path_album}/{audiofile.tag.artist}/{audiofile.tag.album}")  # if есть директория, то пропустить
+    print(path_album)
+
+    print(os.listdir(path=path_album))
+
+    """Перенести файл в созданную директорию"""
+    os.replace(audiofile_path,
+               f"{path_album}/{audiofile.tag.artist}/{audiofile.tag.album}/{audiofile.tag.artist}" + " - " + f"{audiofile.tag.title}.mp3")
+    """"""
+
+
+if __name__ == "__main__":
+    #create_directory_artist_album("music/Madelyne - Beautiful Child (Hiver & Hammer remix).mp3")
+    music_path = "C:/python/tests/milaamania/music"
+    for i in create_audiofiles_list(music_path):
+        print(i)
+        create_directory_artist_album(i)
+
